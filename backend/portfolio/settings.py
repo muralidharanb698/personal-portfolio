3d4e374577129eae94e5680703ba0793d0d22acc
muralidharanb698-crypto <muralidharanb698@gmail.com>
2026-09-07 21:34:11 +0530

    Add requirements

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'portfolio.urls'
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://personal-portfolio-jkrd.vercel.app",
 ]
 
 TEMPLATES = [
